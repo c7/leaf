965d5a45e6a7f823929946ff57cdb3414349b9ce
Peter Hellberg <peter@c7.se>
2010-06-30 01:13:03 +0200

    Include ? and & in the replacement of the page parameter

--- lib/leaf/view_helpers/sinatra.rb
+++ lib/leaf/view_helpers/sinatra.rb
@@ -7,11 +7,11 @@ Leaf::ViewHelpers::LinkRenderer.class_eval do
   def url(page)
     url = @template.request.url
     if page == 1
-      # strip out page param and trailing ? if it exists
-      url.gsub(/page=[0-9]+/, '').gsub(/\?$/, '')
+      # strip out page param and trailing ? and & if they exists
+      url.gsub(/(\?|\&)page=[0-9]+/, '').gsub(/\?$/, '').gsub(/\&$/, '')
     else
-      if url =~ /page=[0-9]+/
-        url.gsub(/page=[0-9]+/, "page=#{page}")
+      if url =~ /(\?|\&)page=[0-9]+/
+        url.gsub(/page=[0-9]+/, "page=#{page}").gsub(/\&+/, '&')
       else
         (url =~ /\?/) ? url + "&page=#{page}" : url + "?page=#{page}"
       end
